3cdc369d4a760bac105dc8aedf1964e943c77f41
Autumnsky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2023-02-15 23:46:36 +0900

    プロジェクト ファイルを追加します。

---
+++ InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures;
+internal class InsertionSort : IExecutable
+{
+    public void Execute()
+    {
+        Console.ReadLine();
+        var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Sort(array);
+    }
+
+    private void Sort(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            int v = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > v)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = v;
+            System.Console.WriteLine(string.Join(' ', array));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmsAndDataStructures;
+public class Program
+{
+    private static void Main()
+    {
+        Console.ReadLine();
+        var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        InsertionSort(array);
+    }
+    private static void InsertionSort(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            int v = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > v)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = v;
+            System.Console.WriteLine(string.Join(' ', array));
+        }
+    }
+}
